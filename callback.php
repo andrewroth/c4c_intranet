04883a2463ce6bcb9740d3e3fe8b02895b5a6877
Adam Kerr <ajrkerr@gmail.com>
2009-06-02 12:17:17 -0700

    Removed displaying of PHP errors.

--- callback.php
+++ callback.php
@@ -1,5 +1,4 @@
 <?php
-ini_set("display_errors", 1);
 require("General/gen_Includes.php");
 
 CASUser::setup("/var/www/campus/dev.intranet.campusforchrist.org/callback.log");
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-ini_set("display_errors", 1);
 require("General/gen_Includes.php");
 
 // get the proper page template to use in displaying the data.
